acc12ad2a0edc00432575f279b71293dd75dba9f
Luke Crouch <lukaswheldon@yahoo.de>
2020-11-11 10:02:26 +0100

    Initial Commit

---
+++ iAlti v2 WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  iAlti v2 WatchKit Extension
+//
+//  Created by Lukas Wheldon on 11.11.20.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
